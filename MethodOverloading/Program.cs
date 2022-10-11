b6b539104a2bb07dbb48c43250486681250eaa9d
Elijah Kariuki <ENKariuki@edfinancial.com>
2022-10-11 05:52:46 -0400

    initial commit

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace MethodOverloading
+﻿using System.ComponentModel;
+
+namespace MethodOverloading
 {
     internal class Program
     {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static decimal Add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+        public static string Add(int a, int b, bool c)
+        {
+            int sum = a + b;
+            if (c == true)
+            {
+
+                if (sum > 1)
+                {
+                    return $"The sum is {sum} dollars";
+                }
+                else
+                    return $"The sum is {sum} dollar";
+            }
+            return $"The sum of {a} plus {b} is {sum}";
+
+        }   
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(32.0m, .7m));
         }
+        
     }
 }
